c566d6f9141b58119b5187a3d6870fc4be50d911
Juan Franco Suarez <frann.suarez@gmail.com>
2018-03-27 16:41:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zktecoF21FTP
---
+++ zkteco.py
@@ -0,0 +1,36 @@
+import socket
+import telnetlib
+import sys
+from ftplib import FTP
+import os
+
+USER = 'root'
+HOST = '10.14.0.41'
+PASSWORD = 'solokey'
+DIRECTORY = 'bad'
+LOCALDIR = '/var/www/biometrico.slyt.local/img/yrigoyen'
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(10)
+result = sock.connect_ex((HOST,21))
+sock.settimeout(None)
+if result == 0:
+   ftp = FTP(HOST)
+   ftp.login()
+   ftp.cwd(DIRECTORY)
+   for filename in ftp.nlst():
+        localfname = os.path.join(LOCALDIR, filename)
+	print 'Getting ' + filename 
+        fhandle =  open(localfname, 'wb')
+        ftp.retrbinary('RETR ' + filename, fhandle.write)
+        fhandle.close()
+else:
+   tn = telnetlib.Telnet(HOST)
+   tn.read_until("login: ")
+   tn.write(USER + "\n")
+   tn.read_until("Password: ")
+   tn.write(PASSWORD + "\n")
+   tn.write("nohup tcpsvd -vE 0.0.0.0 21 ftpd  /mnt/mtdblock/data/capture/ & \n")
+   tn.write("exit\n")
+   print tn.read_all() 
